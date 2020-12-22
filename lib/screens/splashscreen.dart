79b6de0c7fa8a1a516db3d53b3035e390e085c1d
GabbyOwusu <59026711+GabbyOwusu@users.noreply.github.com>
2020-12-22 16:53:38 +0000

    Created Splash screen

---
+++ lib/screens/splashscreen.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:recharge/screens/onboarding.dart';
+
+class Splash extends StatefulWidget {
+  @override
+  _SplashState createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  bool visible = false;
+
+  @override
+  void initState() {
+    Future.delayed(
+      Duration(milliseconds: 500),
+      () {
+        setState(() => visible = true);
+        Future.delayed(
+          Duration(seconds: 3),
+          () {
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(
+                builder: (context) {
+                  return Onboarding();
+                },
+              ),
+            );
+          },
+        );
+      },
+    );
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: AnimatedOpacity(
+        duration: Duration(milliseconds: 1000),
+        curve: Curves.easeInOut,
+        opacity: visible ? 1 : 0,
+        child: Center(
+          child: RichText(
+            text: TextSpan(
+              style: TextStyle(
+                fontWeight: FontWeight.w600,
+                fontSize: 30,
+              ),
+              children: [
+                TextSpan(
+                  text: 'Re',
+                  style: TextStyle(color: Colors.blue),
+                ),
+                TextSpan(
+                  text: 'charge',
+                  style: TextStyle(color: Colors.green),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
