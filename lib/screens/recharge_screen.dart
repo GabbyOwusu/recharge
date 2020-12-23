f219fd196095adb5ec8050016de9dd32cb624a0f
GabbyOwusu <59026711+GabbyOwusu@users.noreply.github.com>
2020-12-23 10:37:50 +0000

    Created recharge screen

---
+++ lib/screens/recharge_screen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class RechargeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        centerTitle: false,
+        title: Text(
+          'Scan rechrage card',
+          style: TextStyle(
+            color: Colors.black,
+          ),
+        ),
+        backgroundColor: Colors.transparent,
+        automaticallyImplyLeading: false,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(Icons.arrow_back_ios),
+          color: Colors.black,
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('Select your carrier'),
+          ],
+        ),
+      ),
+    );
+  }
+}
