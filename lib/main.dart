9cb1d4fdb05beaf9d858624276874c3bab433d3a
GabbyOwusu <59026711+GabbyOwusu@users.noreply.github.com>
2020-12-23 12:46:33 +0000

    Set global font family

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
       title: 'Recharge',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        fontFamily: 'Montserrat',
       ),
       home: Splash(),
     );
