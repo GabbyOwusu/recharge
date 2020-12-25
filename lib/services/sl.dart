a82afd162c06d23cb8164b2b17afed37f1fde0c4
GabbyOwusu <59026711+GabbyOwusu@users.noreply.github.com>
2020-12-25 17:14:19 +0000

    Registerd ocr class with getIt

--- lib/services/sl.dart
+++ lib/services/sl.dart
@@ -1,8 +1,10 @@
 import 'package:get_it/get_it.dart';
 import 'package:recharge/services/fileservice.dart';
+import 'package:recharge/services/ocr.dart';
 
 GetIt sl = GetIt.instance;
 
 setupServiceLocator() {
   sl.registerLazySingleton<FileService>(() => FileService());
+  sl.registerLazySingleton<OCR>(() => OCR());
 }
