d10e994445660cb7c20630410056c07b627e3812
GabbyOwusu <59026711+GabbyOwusu@users.noreply.github.com>
2020-12-25 16:04:48 +0000

    Added file service

---
+++ lib/providers/file_provider.dart
@@ -0,0 +1,32 @@
+import 'dart:io';
+
+import 'package:recharge/providers/baseprovider.dart';
+import 'package:recharge/services/fileservice.dart';
+import 'package:recharge/services/sl.dart';
+
+class FileProvider extends BaseProvider {
+  File _image;
+  File get image => _image;
+
+  final service = sl.get<FileService>();
+
+  Future getImageFromCamera() async {
+    final picture = await service.cameraImage();
+    if (picture != null) {
+      _image = File(picture.path);
+    } else {
+      print('No image selector');
+    }
+    notifyListeners();
+  }
+
+  Future galleryImage() async {
+    final picture = await service.galleryImage();
+    if (picture != null) {
+      _image = File(picture.path);
+    } else {
+      print('No image selector');
+    }
+    notifyListeners();
+  }
+}
