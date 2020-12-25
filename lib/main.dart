c73015699778a34d240a447e0950581fc1abb423
GabbyOwusu <59026711+GabbyOwusu@users.noreply.github.com>
2020-12-25 16:03:50 +0000

    Injected provider into top of widget tree

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:recharge/providers/file_provider.dart';
 import 'package:recharge/screens/splashscreen.dart';
+import 'package:recharge/services/sl.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  setupServiceLocator();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Recharge',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        fontFamily: 'Montserrat',
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => FileProvider()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Recharge',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          fontFamily: 'Montserrat',
+        ),
+        home: Splash(),
       ),
-      home: Splash(),
     );
   }
 }
