55226fa95b7cc2bfae12d3dd71e0bf6e323070f5
GabbyOwusu <59026711+GabbyOwusu@users.noreply.github.com>
2020-12-25 16:05:32 +0000

    Create file service

---
+++ lib/services/fileservice.dart
@@ -0,0 +1,15 @@
+import 'package:image_picker/image_picker.dart';
+
+class FileService {
+  ImagePicker picker;
+
+  Future<PickedFile> cameraImage() async {
+    final pickedFile = await picker.getImage(source: ImageSource.camera);
+    return pickedFile;
+  }
+
+  Future galleryImage() async {
+    final pickedfile = await picker.getImage(source: ImageSource.gallery);
+    return pickedfile;
+  }
+}
