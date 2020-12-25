ab3d05ae2cdeabf354d5d43a45ba776434757b4e
GabbyOwusu <59026711+GabbyOwusu@users.noreply.github.com>
2020-12-25 16:40:45 +0000

    Update

--- lib/services/fileservice.dart
+++ lib/services/fileservice.dart
@@ -1,7 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:image_picker/image_picker.dart';
 
 class FileService {
-  ImagePicker picker;
+  ImagePicker picker = ImagePicker();
 
   Future<PickedFile> cameraImage() async {
     final pickedFile = await picker.getImage(source: ImageSource.camera);
