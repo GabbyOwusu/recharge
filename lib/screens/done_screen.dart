f33cd0266b1eda5e837cfbc7d37262dbdec5ba87
GabbyOwusu <59026711+GabbyOwusu@users.noreply.github.com>
2020-12-26 20:18:57 +0000

    Update

--- lib/screens/done_screen.dart
+++ lib/screens/done_screen.dart
@@ -1,47 +1,75 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:provider/provider.dart';
+
+import 'package:recharge/providers/file_provider.dart';
 import 'package:recharge/widgets/recharge_button.dart';
 
-class Done extends StatelessWidget {
+class Done extends StatefulWidget {
+  final ImageSource source;
+
+  const Done({Key key, this.source}) : super(key: key);
+  @override
+  _DoneState createState() => _DoneState();
+}
+
+class _DoneState extends State<Done> {
+  FileProvider get provider {
+    return Provider.of<FileProvider>(context);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
+    provider.processImage(widget.source);
     return Scaffold(
       body: SafeArea(
-        child: Center(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'Your Voucher code',
-                style: TextStyle(
-                    fontStyle: FontStyle.italic,
-                    fontWeight: FontWeight.w300,
-                    color: Colors.grey),
-              ),
-              SizedBox(height: 100),
-              Text(
-                '8140 8131 1844 47',
-                style: TextStyle(
-                  fontSize: 30,
+        child: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Your Voucher code',
+                  style: TextStyle(
+                      fontStyle: FontStyle.italic,
+                      fontWeight: FontWeight.w300,
+                      color: Colors.grey),
                 ),
-              ),
-              SizedBox(height: 50),
-              Text('Your voucher was scanned successfully'),
-              SizedBox(height: 50),
-              Container(
-                height: 50,
-                decoration: BoxDecoration(
-                  color: Colors.green,
-                  shape: BoxShape.circle,
-                ),
-                child: Center(
-                  child: Icon(
-                    Icons.done,
-                    color: Colors.white,
+                SizedBox(height: 100),
+                Text(
+                  provider.extracted,
+                  style: TextStyle(
+                    fontSize: 30,
                   ),
                 ),
-              ),
-            ],
+                SizedBox(height: 50),
+                Text(
+                  provider.extracted == ''
+                      ? 'Your voucher was scanned successfully'
+                      : 'Sorry rescan image',
+                ),
+                SizedBox(height: 50),
+                Container(
+                  height: 50,
+                  decoration: BoxDecoration(
+                    color: Colors.green,
+                    shape: BoxShape.circle,
+                  ),
+                  child: Center(
+                    child: Icon(
+                      Icons.done,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
