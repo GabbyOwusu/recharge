fc639a27dd8c57be8d69356fc235d23b1c76555b
GabbyOwusu <59026711+GabbyOwusu@users.noreply.github.com>
2020-12-28 19:15:21 +0000

    Update

--- lib/widgets/recharge_button.dart
+++ lib/widgets/recharge_button.dart
@@ -19,24 +19,21 @@ class RechargeButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: ontapped,
-      child: Expanded(
-        flex: 1,
-        child: Container(
-          margin: EdgeInsets.all(30),
-          height: 70,
-          width: 100,
-          decoration: BoxDecoration(
-            color: buttonColor,
-            borderRadius: BorderRadius.circular(10),
-            border: Border.all(color: borderColor),
-          ),
-          child: Center(
-            child: Text(
-              buttonLabel,
-              style: TextStyle(
-                color: textColor,
-                fontSize: 18,
-              ),
+      child: Container(
+        margin: EdgeInsets.all(30),
+        height: 70,
+        width: 100,
+        decoration: BoxDecoration(
+          color: buttonColor,
+          borderRadius: BorderRadius.circular(10),
+          border: Border.all(color: borderColor),
+        ),
+        child: Center(
+          child: Text(
+            buttonLabel,
+            style: TextStyle(
+              color: textColor,
+              fontSize: 18,
             ),
           ),
         ),
