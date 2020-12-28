632b541de1407c104db985890c7f267ce67cead1
GabbyOwusu <59026711+GabbyOwusu@users.noreply.github.com>
2020-12-28 19:14:58 +0000

    Update

--- lib/screens/done_screen.dart
+++ lib/screens/done_screen.dart
@@ -1,14 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
 import 'package:provider/provider.dart';
 
 import 'package:recharge/providers/file_provider.dart';
 import 'package:recharge/widgets/recharge_button.dart';
 
 class Done extends StatefulWidget {
-  final ImageSource source;
-
-  const Done({Key key, this.source}) : super(key: key);
   @override
   _DoneState createState() => _DoneState();
 }
@@ -19,86 +15,86 @@ class _DoneState extends State<Done> {
   }
 
   @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    provider.processImage(widget.source);
     return Scaffold(
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Center(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Spacer(),
+            Text(
+              'Your Voucher code',
+              style: TextStyle(
+                fontStyle: FontStyle.italic,
+                fontWeight: FontWeight.w300,
+                color: Colors.grey,
+              ),
+            ),
+            SizedBox(height: 100),
+            Text(
+              provider.extracted == ''
+                  ? 'Your voucher will appear here'
+                  : provider.extracted,
+              overflow: TextOverflow.ellipsis,
+              style: TextStyle(
+                fontSize: 20,
+              ),
+            ),
+            SizedBox(height: 50),
+            Text(
+              provider.extracted != ''
+                  ? 'Your voucher was scanned successfully'
+                  : 'Sorry rescan image',
+            ),
+            SizedBox(height: 50),
+            Container(
+              height: 50,
+              decoration: BoxDecoration(
+                color: provider.extracted == ''
+                    ? Colors.transparent
+                    : Colors.green,
+                shape: BoxShape.circle,
+              ),
+              child: Center(
+                child: Icon(
+                  provider.extracted == '' ? Icons.cancel_outlined : Icons.done,
+                  color: provider.extracted == '' ? Colors.red : Colors.white,
+                ),
+              ),
+            ),
+            Spacer(),
+            Row(
               children: [
-                Text(
-                  'Your Voucher code',
-                  style: TextStyle(
-                      fontStyle: FontStyle.italic,
-                      fontWeight: FontWeight.w300,
-                      color: Colors.grey),
-                ),
-                SizedBox(height: 100),
-                Text(
-                  provider.extracted,
-                  style: TextStyle(
-                    fontSize: 30,
+                Expanded(
+                  flex: 1,
+                  child: RechargeButton(
+                    buttonColor: Colors.green,
+                    borderColor: Colors.transparent,
+                    textColor: Colors.white,
+                    buttonLabel: 'Recharge',
+                    ontapped: () {},
                   ),
                 ),
-                SizedBox(height: 50),
-                Text(
-                  provider.extracted == ''
-                      ? 'Your voucher was scanned successfully'
-                      : 'Sorry rescan image',
-                ),
-                SizedBox(height: 50),
-                Container(
-                  height: 50,
-                  decoration: BoxDecoration(
-                    color: Colors.green,
-                    shape: BoxShape.circle,
-                  ),
-                  child: Center(
-                    child: Icon(
-                      Icons.done,
-                      color: Colors.white,
-                    ),
+                Expanded(
+                  flex: 1,
+                  child: RechargeButton(
+                    buttonColor: Colors.transparent,
+                    borderColor: Colors.green,
+                    textColor: Colors.green,
+                    buttonLabel: 'Share',
+                    ontapped: () {},
                   ),
                 ),
               ],
             ),
-          ),
-        ),
-      ),
-      bottomSheet: Container(
-        child: Row(
-          children: [
-            Expanded(
-              flex: 1,
-              child: RechargeButton(
-                buttonColor: Colors.green,
-                borderColor: Colors.transparent,
-                textColor: Colors.white,
-                buttonLabel: 'Recharge',
-                ontapped: () {},
-              ),
-            ),
-            Expanded(
-              flex: 1,
-              child: RechargeButton(
-                buttonColor: Colors.transparent,
-                borderColor: Colors.green,
-                textColor: Colors.green,
-                buttonLabel: 'Share',
-                ontapped: () {},
-              ),
-            ),
           ],
         ),
       ),
+      // bottomSheet: Container(
+
+      //   child:
+      // ),
     );
   }
 }
