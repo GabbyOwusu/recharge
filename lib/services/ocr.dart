cbde9b640d5b370667b1b4f46e29f0ddf4fc238c
GabbyOwusu <59026711+GabbyOwusu@users.noreply.github.com>
2020-12-28 21:44:22 +0000

    Update

--- lib/services/ocr.dart
+++ lib/services/ocr.dart
@@ -7,21 +7,18 @@ class OCR {
   String result = '';
 
   Future readImage({File pickedimage}) async {
-    try {
-      FirebaseVisionImage image = FirebaseVisionImage.fromFile(pickedimage);
-      VisionText text = await textRecognizer.processImage(image);
-      for (TextBlock block in text.blocks) {
-        for (TextLine line in block.lines) {
-          for (TextElement word in line.elements) {
-            print(word.text);
-            result = word.text;
-            print('Extracted OCR text $result');
-            return result;
-          }
-        }
-      }
-    } catch (e) {
-      print('Ocr failed because $e');
-    }
+    FirebaseVisionImage image = FirebaseVisionImage.fromFile(pickedimage);
+    VisionText text = await textRecognizer.processImage(image);
+    print('Texts..........${text.text}');
+    // for (TextBlock block in text.blocks) {
+    //   for (TextLine line in block.lines) {
+    //     for (TextElement word in line.elements) {
+    //       print(word.text);
+    //       result = word.text;
+    //       print('Extracted OCR text $result');
+    //       // return result;
+    //     }
+    //   }
+    // }
   }
 }
