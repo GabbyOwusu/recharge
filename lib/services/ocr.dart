6e922ee81415c774d5df9709d8bffd27c1e092e9
GabbyOwusu <59026711+GabbyOwusu@users.noreply.github.com>
2020-12-29 17:56:22 +0000

    Update

--- lib/services/ocr.dart
+++ lib/services/ocr.dart
@@ -1,24 +1,15 @@
-import 'dart:io';
+// import 'dart:io';
 
-import 'package:firebase_ml_vision/firebase_ml_vision.dart';
+// import 'package:firebase_ml_vision/firebase_ml_vision.dart';
 
-class OCR {
-  TextRecognizer textRecognizer = FirebaseVision.instance.textRecognizer();
-  String result = '';
+// class OCR {
+//   TextRecognizer textRecognizer = FirebaseVision.instance.textRecognizer();
+//   String result = '';
 
-  Future readImage({File pickedimage}) async {
-    FirebaseVisionImage image = FirebaseVisionImage.fromFile(pickedimage);
-    VisionText text = await textRecognizer.processImage(image);
-    print('Texts..........${text.text}');
-    // for (TextBlock block in text.blocks) {
-    //   for (TextLine line in block.lines) {
-    //     for (TextElement word in line.elements) {
-    //       print(word.text);
-    //       result = word.text;
-    //       print('Extracted OCR text $result');
-    //       // return result;
-    //     }
-    //   }
-    // }
-  }
-}
+//   Future readImage({File pickedimage}) async {
+//     FirebaseVisionImage image = FirebaseVisionImage.fromFile(pickedimage);
+//     VisionText text = await textRecognizer.processImage(image);
+//     print('Texts..........${text.text}');
+
+//   }
+// }
