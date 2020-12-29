eac94153b69057168700d14103b040779560acb6
GabbyOwusu <59026711+GabbyOwusu@users.noreply.github.com>
2020-12-29 17:56:02 +0000

    Update

--- lib/providers/file_provider.dart
+++ lib/providers/file_provider.dart
@@ -1,9 +1,9 @@
 import 'dart:io';
 
+import 'package:firebase_ml_vision/firebase_ml_vision.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:recharge/providers/baseprovider.dart';
 import 'package:recharge/services/fileservice.dart';
-import 'package:recharge/services/ocr.dart';
 import 'package:recharge/services/sl.dart';
 import 'package:ussd/ussd.dart';
 
@@ -12,9 +12,9 @@ class FileProvider extends BaseProvider {
 
   String _extractedText = '';
   String get extracted => _extractedText;
+  TextRecognizer textRecognizer = FirebaseVision.instance.textRecognizer();
 
   final service = sl.get<FileService>();
-  final ocr = sl.get<OCR>();
 
   Future<File> getImage(ImageSource imagesorce) async {
     PickedFile picture = await service.cameraImage(imagesorce);
@@ -28,13 +28,23 @@ class FileProvider extends BaseProvider {
 
   Future processImage(ImageSource source) async {
     File visionimage = await getImage(source);
-    final text = await ocr.readImage(pickedimage: File(visionimage.path));
-    if (text == null) {
-      print('Sorry no text was obtained');
-    } else {
-      _extractedText = text.text;
+    FirebaseVisionImage image = FirebaseVisionImage.fromFile(visionimage);
+    VisionText text = await textRecognizer.processImage(image);
+    for (TextBlock block in text.blocks) {
+      final blocknumbers = block.text.replaceAll(new RegExp(r'[^0-9]'), '');
+      if (blocknumbers.length == 14) {
+        _extractedText = blocknumbers;
+        print('Voucher digits here .....$_extractedText');
+      } else if (blocknumbers.length > 14 || blocknumbers.length < 14) {
+        _extractedText = 'Your voucher digits exceeds limit';
+        print('$blocknumbers is more than 14 digits');
+      }
+    }
+    if (text.text == null) {
+      _extractedText = '';
     }
     notifyListeners();
+    // textRecognizer.close();
   }
 
   Future<void> runUssd(String code) async {
