1b256ab372f9b64a65a60a31e117d90444ff0b9f
GabbyOwusu <59026711+GabbyOwusu@users.noreply.github.com>
2020-12-31 22:37:44 +0000

    Update

--- lib/screens/splashscreen.dart
+++ lib/screens/splashscreen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:recharge/screens/home.dart';
 import 'package:recharge/screens/onboarding.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Splash extends StatefulWidget {
   @override
@@ -8,6 +10,23 @@ class Splash extends StatefulWidget {
 
 class _SplashState extends State<Splash> {
   bool visible = false;
+
+  Future checkFirstSeen() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    bool _seen = (prefs.getBool('seen') ?? false);
+
+    if (_seen) {
+      Navigator.of(context).pushReplacement(
+          new MaterialPageRoute(builder: (context) => MyHomePage()));
+    } else {
+      await prefs.setBool('seen', true);
+      Navigator.of(context).pushReplacement(
+          new MaterialPageRoute(builder: (context) => new Onboarding()));
+    }
+  }
+
+  @override
+  void afterFirstLayout(BuildContext context) => checkFirstSeen();
 
   @override
   void initState() {
