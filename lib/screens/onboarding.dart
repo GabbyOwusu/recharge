aa90daffaeb01b0572911ca84bafada43f2da8e9
GabbyOwusu <59026711+GabbyOwusu@users.noreply.github.com>
2020-12-31 13:02:03 +0000

    Update

--- lib/screens/onboarding.dart
+++ lib/screens/onboarding.dart
@@ -1,76 +1,121 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 import 'package:recharge/screens/home.dart';
+import 'package:recharge/widgets/pageview_child.dart';
 
-class Onboarding extends StatelessWidget {
+class Onboarding extends StatefulWidget {
+  @override
+  _OnboardingState createState() => _OnboardingState();
+}
+
+class _OnboardingState extends State<Onboarding> {
+  int _currentIndex = 0;
+
+  Widget dots() {
+    final activeDot = AnimatedContainer(
+      duration: Duration(milliseconds: 300),
+      height: 10,
+      width: 20,
+      margin: EdgeInsets.only(right: 5),
+      decoration: BoxDecoration(
+        color: Colors.green,
+        borderRadius: BorderRadius.circular(5),
+      ),
+    );
+    final inactiveDot = AnimatedContainer(
+      duration: Duration(milliseconds: 300),
+      height: 10,
+      width: 10,
+      margin: EdgeInsets.only(right: 7),
+      decoration: BoxDecoration(
+        color: Colors.green[100],
+        borderRadius: BorderRadius.circular(5),
+      ),
+    );
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: List<Widget>.generate(2, (index) {
+        if (index == _currentIndex)
+          return activeDot;
+        else
+          return inactiveDot;
+      }),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              'images/illustration.png',
-              width: 250,
-            ),
-            SizedBox(height: 50),
-            Text(
-              'Welcome',
-              style: TextStyle(
-                fontSize: 30,
-                fontWeight: FontWeight.w400,
+      body: Stack(
+        children: [
+          PageView(
+            onPageChanged: (index) {
+              setState(() {
+                _currentIndex = index;
+              });
+            },
+            children: [
+              PageViewChild(
+                title: 'Welcome',
+                image: 'images/illustration.png',
+                description: 'An easier and more convienient way of ' +
+                    'topping up \nairtimefrom recharge cards. ' +
+                    'Scratch the card gently.\nTake a clear picture of ' +
+                    'the section containing the\nvoucher digits.',
               ),
+              PageViewChild(
+                title: 'Get Started',
+                image: 'images/card.png',
+                description:
+                    '''Thats it!. In a few easy steps, you can top up your \nairtime or easily
+                     share voucher code with
+                     friends and family. Get started.''',
+              ),
+            ],
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: EdgeInsets.only(bottom: 120),
+              child: dots(),
             ),
-            SizedBox(height: 50),
-            Text(
-              'Lorem ipsum dolor sit amet, consectetur' +
-                  'adipiscing \nelit, sed do eiusmod tempor' +
-                  'incididunt ut labore\n et dolore magna aliqua.' +
-                  'Ut enim ad minim',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Colors.grey,
-                fontSize: 15,
-                fontWeight: FontWeight.w300,
+          )
+        ],
+      ),
+      bottomSheet: _currentIndex == 1
+          ? GestureDetector(
+              onTap: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return MyHomePage();
+                    },
+                  ),
+                );
+              },
+              child: AnimatedContainer(
+                duration: Duration(seconds: 2),
+                margin: EdgeInsets.only(bottom: 30, left: 40, right: 40),
+                width: double.infinity,
+                height: 60,
+                decoration: BoxDecoration(
+                  color: Colors.green,
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                child: Center(
+                  child: Text(
+                    'Get Started',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
               ),
             )
-          ],
-        ),
-      ),
-      bottomSheet: GestureDetector(
-        onTap: () {
-          Navigator.pushReplacement(
-            context,
-            MaterialPageRoute(
-              builder: (context) {
-                return MyHomePage();
-              },
-            ),
-          );
-        },
-        child: Container(
-          margin: EdgeInsets.only(bottom: 30, left: 40, right: 40),
-          width: double.infinity,
-          height: 60,
-          decoration: BoxDecoration(
-            color: Colors.green,
-            borderRadius: BorderRadius.circular(30),
-          ),
-          child: Center(
-            child: Text(
-              'Get Started',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 20,
-                fontWeight: FontWeight.w600,
-              ),
-            ),
-          ),
-        ),
-      ),
+          : Text(''),
     );
   }
 }
